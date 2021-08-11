6b81d440b28a3ba51f5c992cad398ee48ca3e3de
Guadalupe <guadalupe.ines.alavila@everis.com>
2021-08-11 17:44:19 -0300

    changes

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     transform: {'^.+\\.ts?$': 'ts-jest'},
+    roots: ['<rootDir>/src'],
+    preset: 'ts-jest',
     testEnvironment: 'node',
-    // testRegex: '/tests/.*\\.(test|spec)?\\.(ts)$',
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: ['ts', 'js', 'json', 'node']
+    testRegex: '/tests/.*\\.(test|spec)?\\.(ts)$',
+    // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
+    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+    testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+    coveragePathIgnorePatterns: ['/node_modules/', '.+\\.(css|styl|less|sass|scss)$'],
 };
