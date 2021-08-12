ed282d0d0438279ccf82a796fbf156c6821c8d21
Guadalupe <guadalupe.ines.alavila@everis.com>
2021-08-11 21:03:26 -0300

    change

--- src/firebase/getData.ts
+++ src/firebase/getData.ts
@@ -1,12 +1,24 @@
 /* eslint-disable no-console */
 import firebase from 'firebase/app';
+interface FirebaseConfiguration {
+    apiKey: string,
+    authDomain: string,
+    projectId: string,
+    storageBucket: string,
+    messagingSenderId: string,
+    appId: string,
+    measurementId: string,
+}
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export function getData() {
+export function getData(firebaseConfig: FirebaseConfiguration) {
     // const remoteConfig = firebase.remoteConfig();
     // remoteConfig.defaultConfig = {
     //     welcome_message: 'Welcome',
     // };
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    }
     firebase
         .remoteConfig()
         .fetchAndActivate()
@@ -22,7 +34,10 @@ export function getData() {
 }
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function fetchKey(key: string) {
+export async function fetchKey(firebaseConfig: FirebaseConfiguration, key: string) {
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    }
     await firebase.remoteConfig().fetchAndActivate();
     return firebase.remoteConfig().getString(key);
 }
