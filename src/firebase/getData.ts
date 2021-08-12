e3e70ede220a3a1467ec61a3f973bfa96389b8a4
Guadalupe <guadalupe.ines.alavila@everis.com>
2021-08-11 21:27:27 -0300

    test

--- src/firebase/getData.ts
+++ src/firebase/getData.ts
@@ -1,5 +1,5 @@
-/* eslint-disable no-console */
 import firebase from 'firebase/app';
+import 'firebase/remote-config';
 interface FirebaseConfiguration {
     apiKey: string,
     authDomain: string,
@@ -11,43 +11,22 @@ interface FirebaseConfiguration {
 }
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export function getData(firebaseConfig: FirebaseConfiguration) {
-    // const remoteConfig = firebase.remoteConfig();
-    // remoteConfig.defaultConfig = {
-    //     welcome_message: 'Welcome',
-    // };
-
+export function getAllData(firebaseConfig: FirebaseConfiguration) {
     if (!firebase.apps.length) {
         firebase.initializeApp(firebaseConfig);
     }
-    const remoteConfig = firebase.remoteConfig();
-
-    remoteConfig.settings = {
-        minimumFetchIntervalMillis: 3600,
-        fetchTimeoutMillis: 500
-    };
-    firebase
-        .remoteConfig()
-        .fetchAndActivate()
-        .then((data) => {
-            console.log('hola');
-            console.log(data);
-            return data;
-        })
-        .catch((err) => {
-            console.log('errr');
-            console.log(err);
-            throw err;
-        });
+    const remoteConfig =  firebase.remoteConfig();
+    console.log(remoteConfig.getAll());
+    return remoteConfig.getAll();
 }
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function fetchKey(firebaseConfig: FirebaseConfiguration, key: string) {
+export function fetchKey(firebaseConfig: FirebaseConfiguration, key: string) {
     if (!firebase.apps.length) {
         firebase.initializeApp(firebaseConfig);
     }
-    await firebase.remoteConfig().fetchAndActivate();
-    return firebase.remoteConfig().getString(key);
+    const remoteConfig =  firebase.remoteConfig();
+    return remoteConfig.getValue(key);
 }
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { add } from './math/add';
 import { subtract } from './math/subtract';
 import { initFirebase } from './firebase/init';
-import { getData, fetchKey } from './firebase/getData';
+import { getAllData, fetchKey } from './firebase/getData';
 
 
-export {add, subtract, initFirebase, getData, fetchKey};
+export {add, subtract, initFirebase, getAllData, fetchKey};
