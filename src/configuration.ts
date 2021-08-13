7bde3b09c6ae3547ce4b985cd327d58f2fc27995
Guadalupe <guadalupe.ines.alavila@everis.com>
2021-08-13 12:06:51 -0300

    tests

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,29 +1,46 @@
-
+/* eslint-disable @typescript-eslint/no-empty-function */
 class Configuration {
+    constructor() {}
 
-    private static instance: Configuration;
-
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    private constructor() { }
-   
-    public static getInstance(): Configuration {
-        if (!Configuration.instance) {
-            Configuration.instance = new Configuration();
-        }
-        return Configuration.instance;
-    }
-
-    public static getProp1(): string {
+    method1() {
         return 'prop1';
     }
-    public static getProp2(): string {
-        return 'prop1';
-    }
-    public static  initConfiguration(): void {
-        console.log('Se inicia SDK');
-        // throw new Error('Method not implemented.');
-    }
 
+    method2() {
+        return 'prop2';
+    }
 }
 
-export default Configuration;
+const configurationInstance = new Configuration();
+
+Object.freeze(configurationInstance);
+export default configurationInstance;
+
+// class Configuration {
+
+//     private static instance: Configuration;
+
+//     // eslint-disable-next-line @typescript-eslint/no-empty-function
+//     private constructor() { }
+
+//     public static getInstance(): Configuration {
+//         if (!Configuration.instance) {
+//             Configuration.instance = new Configuration();
+//         }
+//         return Configuration.instance;
+//     }
+
+//     public static getProp1(): string {
+//         return 'prop1';
+//     }
+//     public static getProp2(): string {
+//         return 'prop1';
+//     }
+//     public static  initConfiguration(): void {
+//         console.log('Se inicia SDK');
+//         // throw new Error('Method not implemented.');
+//     }
+
+// }
+
+// export default configurationInstance;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { add } from './math/add';
 import { subtract } from './math/subtract';
 import { fetchAllData, fetchStringValue, fetchBooleanValue, fetchNumberValue } from './utils/functions';
-import * as Configuration from './configuration';
+import configurationInstance from './configuration';
 
-export {add, subtract, fetchAllData, fetchStringValue, fetchBooleanValue, fetchNumberValue, Configuration};
+export {add, subtract, fetchAllData, fetchStringValue, fetchBooleanValue, fetchNumberValue, configurationInstance};
